36c49581a0e820623fe10c2a3ffe55954e1baf96
maxpotiatynok <c5030576@newcastle.ac.uk>
2026-09-07 21:43:58 +0100

    Created a database, a schema and the method to sort API response to the table

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS Disruptions (
         Line TEXT NOT NULL,
-        Category TEXT,
-        DType TEXT,
-        CategoryDescription TEXT,
-        Description TEXT,
-        Summary TEXT,
-        AdditionalInfo TEXT,
-        Created DATETIME,
-        APILastUpdated TEXT,
-        ClientLastUpdated TEXT,
-        PRIMARY KEY(Line, Category, ClientLastUpdated)
+        LineName TEXT,
+        StatusSeverity TEXT NOT NULL,
+        SeverityDescription TEXT,
+        Reason TEXT,
+        FromDate TEXT,
+        ToDate TEXT,
+        DisruptionCategory TEXT,
+        DisruptionClosureText TEXT,
+        FetchDate TEXT,
+        UNIQUE (Line,StatusSeverity, FromDate, DisruptionCategory)
     );
--- tfl_extractor.py
+++ tfl_extractor.py
@@ -1,3 +1,4 @@
+from datetime import timezone, datetime
 from pathlib import Path
 import apiclient
 import os
@@ -7,22 +8,28 @@ BASE_URL = "https://api.tfl.gov.uk"
 API_KEY = os.environ.get("API_KEY")
 tfl_client = apiclient.APIClient(BASE_URL, API_KEY)
 
-conn = sqlite3.connect("tfl.db")
-tfl_cursor = conn.cursor()
-schema = Path("schema.sql").read_text()
-tfl_cursor.execute(schema)
-conn.commit()
-conn.close()
-
+def create_table():
+    """
+    Establishes a connection to the TfL database and creates a table using the schema file
+    """
+    conn = sqlite3.connect("tfl.db")
+    tfl_cursor = conn.cursor()
+    schema = Path("schema.sql").read_text()
+    tfl_cursor.execute(schema)
+    conn.commit()
+    conn.close()
 
 def get_lines_disruptions():
+    """
+    All TfL lines statutes are pulled from the API using the APIClient, the data is sorted to columns and rows in the table
+    """
     disruptions_list = tfl_client.fetch("/Line/Mode/tube/Status")
-    rows = []
+
+    current_time = datetime.now(timezone.utc).isoformat()
 
     for disruption in disruptions_list:
-        disruption_line = disruption.get("line")
+        disruption_line = disruption.get("id")
         disruption_name = disruption.get("name")
-
         for line_status in disruption["lineStatuses"]:
             disruption_severity = line_status["statusSeverity"]
             disruption_severity_description = line_status["statusSeverityDescription"]
@@ -36,19 +43,26 @@ def get_lines_disruptions():
                     disruption_category = line_status["disruption"]["category"]
                     disruption_closure_text = line_status["disruption"]["closureText"]
 
-                    rows.append({
-                        "line_id": disruption_line,
-                        "line_name": disruption_name,
-                        "status_severity": disruption_severity,
-                        "status_description": disruption_severity_description,
-                        "reason": disruption_reason,
-                        "category": disruption_category,
-                        "closure_text": disruption_closure_text,
-                        "valid_from": disruption_created,
-                        "valid_to": disruption_until,
-                        "is_now": disruption_is_now,
-                    })
+                    disruption_conn = sqlite3.connect("tfl.db")
+                    d_cursor = disruption_conn.cursor()
+                    d_cursor.execute(
+                    '''
+                        INSERT OR IGNORE INTO Disruptions(Line, LineName, StatusSeverity, SeverityDescription,
+                            Reason, FromDate, ToDate,
+                            DisruptionCategory, DisruptionClosureText, FetchDate
+                        )
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (
+                                     disruption_line, disruption_name, disruption_severity,
+                                     disruption_severity_description, disruption_reason,
+                                     disruption_created, disruption_until,
+                                     disruption_category, disruption_closure_text, current_time))
 
+
+                    disruption_conn.commit()
+                    disruption_conn.close()
+
+create_table()
 get_lines_disruptions()
 
 
